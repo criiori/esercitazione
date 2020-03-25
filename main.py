382086ebbe9b36481f5c618d920b001e8936cb0b
criiori <cristiano.iori0iori04@gmail.com>
2020-03-25 14:57:24 +0100

    aggiunto codice di python

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ if b>18:
 	print("sei maggiorenne")
 else:
 	print("torna all'asilo")
+c = input("preferisci vestirti di bianco o di nero? ")
+if c=='bianco':
+	print("non sono d'accordo")
+else:
+	print("hai tutto il mio rispetto") 
 
